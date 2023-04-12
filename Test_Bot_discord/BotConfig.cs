d58753099670d6cccad54236130fb13c1951193d
Alex-Imagine <59845644+Alex-Imagine@users.noreply.github.com>
2023-04-12 09:39:00 +0300

    Update BotConfig.cs

--- Test_Bot_discord/BotConfig.cs
+++ Test_Bot_discord/BotConfig.cs
@@ -11,7 +11,7 @@ namespace Test_Bot_discord
     public static class BotConfig
     {
         public static DiscordClient client;
-        private static readonly string token = "OTg5MTU4NTExNTQ2MTA1OTM2.GLueN4.dgfDFGmsr5L25Xwasq0P90Itfr-y6Jc-Voco34";
+        private static readonly string token = "";
         public static bool isExecutting = false;
         public async static void Configure()
         {
